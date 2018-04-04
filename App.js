0ee91cd5755c9240f1b02439eb5a96fbb4531b74
nico24687 <theonlynico@hotmail.com>
2018-04-04 14:53:49 -0600

    fetching image from facebook

--- App.js
+++ App.js
@@ -2,12 +2,24 @@ import React, { Component } from 'react'
 import {
   StyleSheet,
   View,
+  Image,
+  Text,
 } from 'react-native'
+
+const fbImage = 'https://graph.facebook.com/1637993149777250/picture?height=500'
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.card}>
+        <Image
+          style={{flex:1}}
+          source={{ uri: fbImage}}
+        />
+        <View style={{margin:20}}>
+          <Text stlye={{fontSize:20}}>Miranda, 6</Text>
+          <Text style={{fontSize:15, color:'darkgrey'}}>Chow Chow </Text>
+        </View>
       </View>
     )
   }
@@ -16,6 +28,7 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   card: {
     flex: 1,
+    overflow: 'hidden',
     backgroundColor: 'white',
     margin: 10,
     marginTop: 100,
