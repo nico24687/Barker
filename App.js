b4a85d3fd0aa87f4defa0c7608f01e3e2fcf5ea0
nico24687 <theonlynico@hotmail.com>
2018-04-04 14:37:01 -0600

    up and running

---
+++ App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import {
+  StyleSheet,
+  View,
+} from 'react-native'
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.card}>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  card: {
+    flex: 1,
+    backgroundColor: 'white',
+    margin: 10,
+    marginTop: 100,
+    marginBottom: 100,
+    borderWidth: 1,
+    borderColor: 'lightgrey',
+    borderRadius: 8,
+  },
+})
