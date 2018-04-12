e300f8486d6da3cbf8a3fc24c7f0b3699aea75ee
nico24687 <theonlynico@hotmail.com>
2018-04-12 16:27:45 -0600

    added sliders to profile screen

--- app/screens/profile.js
+++ app/screens/profile.js
@@ -4,16 +4,38 @@ import CircleImage from '../components/circleImage'
 import Slider from 'react-native-multi-slider-cloneable'
 
 export default class Profile extends Component{
+  state={
+    ageRangeValues: [0,16],
+    distnaceValue: [5],
+  }
   render(){
     const {first_name,id} = this.props.user
+    const {ageRangeValues, distnaceValue} = this.state
     return(
       <View style={styles.container}>
         <View style={styles.profile}>
           <CircleImage facebookID={id} size={120}/>
           <Text style={{fontSize: 20}}>{first_name}</Text>
         </View>
+        <View style={styles.label}>
+          <Text>Distance</Text>
+          <Text style={{ color: 'darkgrey' }}>{distnaceValue}km</Text>
+        </View>
         <Slider
-          values={[1,10]}
+          min={1}
+          max={30}
+          values={distnaceValue}
+          onValuesChange={val => this.setState({ distnaceValue: val })}
+        />
+        <View style={styles.label}>
+          <Text>Age Range</Text>
+          <Text style={{color: 'darkgrey'}}>{ageRangeValues.join('-')}</Text>
+        </View>
+        <Slider
+          min={0}
+          max={16}
+          values={ageRangeValues}
+          onValuesChange={val => this.setState({ageRangeValues:val})}
         />
       </View>
     )
@@ -30,4 +52,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  label: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginLeft: 20,
+    marginRight: 50,
+  }
 })
