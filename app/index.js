93cddf3de6c7b3e97d0bd8b740345348ca929629
nico24687 <theonlynico@hotmail.com>
2018-04-14 16:03:03 -0600

    going to the blue chat screen

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ import * as firebase from 'firebase'
 import { StackNavigator } from 'react-navigation'
 import Home from './screens/home'
 import Login from './screens/login'
+import Chat from './screens/chat'
 
 const firebaseConfig = {
   apiKey: "AIzaSyDU-nm14gcgIny0wOc-mh4OORHaS1vAzDo",
@@ -13,6 +14,7 @@ firebase.initializeApp(firebaseConfig)
 const RouteConfigs = {
   Login: {screen: Login},
   Home: {screen: Home},
+  Chat: {screen: Chat},
 }
 
 const StackNavigatorConfig = {
